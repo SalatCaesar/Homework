f5a7b01e5b58c2d43f1b520427d10d9d4f6035e1
SalatCaesar <strutynskiy.ao@gmail.com>
2025-08-16 02:10:43 +0300

    Задание 4. 4/7 решено

--- Homework_4/draft2.py
+++ Homework_4/draft2.py
@@ -2,37 +2,36 @@
   # Потом надо придумать цикл, который перестроит список начиная с элемента n+1
   # В конце цикла удаляем последний элемент, который окажется n
   # while до того, как элемент не останется один
-h = 14
+h = 80
 k = 2
 lst = list(range(1,h+1))
 nlst = []
-i = len(lst)
-i2 = None
+i = len(range(len(lst)))
+i2 = len(range(len(lst)))
 if h >= k:
-    while i >= k:
+    while i > k:
         nlst += lst[k:]
-        nlst += lst [0:k]
+        nlst += lst [:k]
         del nlst[-1]
         lst = nlst
         nlst = []
         i = int(len(lst))
-        print(lst)
-    while i2 != 1:
+    while i2 > 1:
         nlst += lst[k%i:]
-        nlst += lst[0:k%i]
+        nlst += lst[:k%i]
         del nlst[-1]
         lst = nlst
         nlst = []
         i = len(range(len(lst)))
         i2 = len(range(len(lst)))
-        print(lst)
 else:
-    while i2 != 1:
+    while i2 > 1:
         nlst += lst[k % i:]
-        nlst += lst[0:k % i]
+        nlst += lst[:k % i]
         del nlst[-1]
         lst = nlst
+        print(lst)
         nlst = []
         i = len(range(len(lst)))
         i2 = len(range(len(lst)))
-        print(lst)
+print(str(lst))
