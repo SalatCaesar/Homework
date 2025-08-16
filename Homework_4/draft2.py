d15d59cdd61b5985357529519963fada1a618c44
SalatCaesar <strutynskiy.ao@gmail.com>
2025-08-16 23:30:56 +0300

    Задание 4. 7/7 решено

--- Homework_4/draft2.py
+++ Homework_4/draft2.py
@@ -1,8 +1,3 @@
-lst = [0 ,1, 0, 2, 0, 3, 0]
-p = lst.count(0)
-for i in range(p):
-    lst.remove(0)
-print(lst)
-for i in range(p):
-    lst.append(0)
-print(lst)
+num = (1, 1, 1, 2, 2, 2, 3, 3, 3, 3)
+p = "({}{}{}) {}{}{}-{}{}{}{}".format(*num)
+print(p)
