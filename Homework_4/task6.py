e32d0e56429ccffe1a8353d8ee0b55e0b354d5fb
SalatCaesar <strutynskiy.ao@gmail.com>
2025-08-16 21:43:45 +0300

    Задание 4. 6/7 решено

--- Homework_4/task6.py
+++ Homework_4/task6.py
@@ -5,6 +5,17 @@
 
 def create_phone_number(num_tuple):
     # Здесь нужно написать код
+    tpl = num_tuple
+    numb = '('
+    for i in range(3):
+        numb = (numb + str(tpl[i]))
+    numb += ') '
+    for i in range(3, 6):
+        numb = (numb + str(tpl[i]))
+    numb += '-'
+    for i in range(6, 10):
+        numb = (numb + str(tpl[i]))
+    str_phone = numb
     return str_phone
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
