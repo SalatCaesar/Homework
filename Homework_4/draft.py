55f9e45c38f22d182da138f182c03b9b340cbcd3
SalatCaesar <strutynskiy.ao@gmail.com>
2025-08-17 19:27:18 +0300

    Задание 4. 7/7 решено Со звёздочкой решено

--- Homework_4/draft.py
+++ Homework_4/draft.py
@@ -108,3 +108,27 @@ numb += '-'
 for i in range(6,10):
     numb = (numb + str(tpl[i]))
 print(numb)
+
+print()
+
+num = 888
+num = str(num)
+p = None
+c = 9
+i = 0
+b = int(num)
+while p != 0:
+    if int(b) < int(num):
+        i += 1
+        c = 9
+    b = str(num[:i]) + str(c) + str(num[i+1:])
+    c -= 1
+    if b == str(num):
+        continue
+    if c == 0:
+        i += 1
+        c = 9
+    p = 0
+    p += int(b)
+    p = p % 3
+print('Число = ', b)
