c72aa12c22a791b1184573af65ab8d3a14496ea2
ao.strutynskiy <ao.strutynskiy@tensor.ru>
2025-11-20 15:43:41 +0300

    Homework 5. 3/4

--- Homework_7/draft_task_3.py
+++ Homework_7/draft_task_3.py
@@ -6,28 +6,43 @@ class PublicTransport:
         self.color = color
         self.max_speed = max_speed
 
-
+    @property
+    def info(self):
+        print(self.brand, self.color, self.year, self._engine_power)
 
 class Bus(PublicTransport):
+    def __init__(self, brand, engine_power, year, color, max_speed, passengers, park, fare):
+        super().__init__(brand, engine_power, year, color, max_speed)
+        self.passengers = passengers
+        self.__park = park
+        self._fare = fare
 
+    @property
+    def park(self):
+        return self.__park
 
+    @park.setter
+    def park(self, number):
+        if 9999 >= number >= 1000:
+            self.__park = number
+        else:
+            print("Номер не верный")
 
 class Tram(PublicTransport):
+    def __init__(self, brand, engine_power, year, color, max_speed, route, path, fare):
+        super().__init__(brand, engine_power, year, color, max_speed)
+        self.__route = route
+        self.path = path
+        self._fare = fare
+    @property
+    def how_long(self):
+        time_route = self.max_speed / (4 * self.path)
+        return time_route
 
+transport = PublicTransport('Автомобиль', 500, 2040, 'Фиолетовый', 300)
+first_bus = Bus('ЛиАЗ', 210, 2015, 'Зеленый', 100, 70, 1232, 32)
+second_bus = Bus('VOLGABUS', 320, 2019, 'Желтый', 110, 39, 1111, 32)
+first_tram = Tram('71-931M', 125, 2010, 'Красный', 75, 5, 15, 32)
+second_tram = Tram('71-409-1', 240, 2018, 'Белый', 85, 7, 17, 32)
 
-
-
-
-
-
-
-
-
-
-
-
-# transport = PublicTransport('Автомобиль', 500, 2040, 'Фиолетовый', 300)
-# first_bus = Bus('ЛиАЗ', 210, 2015, 'Зеленый', 100, 70, 1232, 32)
-# second_bus = Bus('VOLGABUS', 320, 2019, 'Желтый', 110, 39, 1111, 32)
-# first_tram = Tram('71-931M', 125, 2010, 'Красный', 75, 5, 15, 32)
-# second_tram = Tram('71-409-1', 240, 2018, 'Белый', 85, 7, 17, 32)
+print(first_tram.how_long)
