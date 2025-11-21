b630a553fff68a32efe31587f3020ce5bafd9010
SalatCaesar <strutynskiy.ao@gmail.com>
2025-11-21 14:46:03 +0300

    Homework 5. 3/4

---
+++ Homework_7/draft_task_star.py
@@ -0,0 +1,15 @@
+import roman
+
+class RomanNums:
+    def __init__(self, nums):
+        self.nums = nums
+
+    def from_roman(self):
+        return roman.fromRoman(self.nums)
+
+    def is_palindrome(self):
+        def is_palindrome(self):
+            a = str(roman.fromRoman(self.nums))
+            b = str(roman.fromRoman(self.nums))
+            c = b[::-1]
+            return a == c
--- Homework_7/task_with_star.py
+++ Homework_7/task_with_star.py
@@ -9,7 +9,20 @@
 # RomanNums('CMXCIX').is_palindrome() --> True
 
 # Здесь пишем код
+import roman
 
+class RomanNums:
+    def __init__(self, nums):
+        self.nums = nums
+
+    def from_roman(self):
+        return roman.fromRoman(self.nums)
+
+    def is_palindrome(self):
+        a = str(roman.fromRoman(self.nums))
+        b = str(roman.fromRoman(self.nums))
+        c = b[::-1]
+        return a == c
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
 
