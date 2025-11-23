d021ca7a6ae801efa5778dc07e529cf8b4630169
SalatCaesar <strutynskiy.ao@gmail.com>
2025-11-23 22:28:02 +0300

    Homework 9. 1/3

--- Homework_9/draft_task_1.py
+++ Homework_9/draft_task_1.py
@@ -1,9 +1,13 @@
-f = open(r'test_file\task1_data.txt', 'r', encoding='utf-8')
-z = f.readlines()
-f.close()
-r = str()
-for i in z:
-    r += i.translate(str.maketrans('', '', '0123456789'))
-f = open(r'test_file\task1_answer.txt', 'w+', encoding='utf-8')
-f.writelines(r)
-print(*f)
+# Можно улучшить:
+# 1. ПРочитать подробнее про str.translate(). Выглядит, что это может прям много очень
+
+table = str.maketrans('','','1234567890')
+
+with open(r'test_file\task1_data.txt', 'r', encoding='utf-8') as file:
+    file = file.read()
+print(file)
+
+res = file.translate(table)
+
+with open(r'test_file\task1_answer.txt', 'w', encoding='utf-8') as file:
+    file.write(res)
--- Homework_9/task_1.py
+++ Homework_9/task_1.py
@@ -4,7 +4,15 @@
 
 
 # Здесь пишем код
+table = str.maketrans('','','1234567890')
 
+with open(r'test_file\task1_data.txt', 'r', encoding='utf-8') as file:
+    file = file.read()
+
+res = file.translate(table)
+
+with open(r'test_file\task1_answer.txt', 'w', encoding='utf-8') as file:
+    file.write(res)
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
 
