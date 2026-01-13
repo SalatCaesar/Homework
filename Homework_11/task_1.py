e70d602611d9b9b9666eb3429c9df67f4f0e9f89
SalatCaesar <strutynskiy.ao@gmail.com>
2026-01-13 22:58:52 +0300

    Homework 11. 1/3

--- Homework_11/task_1.py
+++ Homework_11/task_1.py
@@ -10,40 +10,50 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 browser = webdriver.Chrome()
-saby_site = "https://saby.ru/"
-
-time.sleep(2)
+site_saby = "https://saby.ru/"
+# Добавил после каждого действия ожидание, а то ничего на видео непонятно
 try:
-    browser.get("https://saby.ru/")
-    time.sleep(2)
+    # Открываем браузер.
+    # Хочу видеть полноэкранный режим
+
+    browser.get(site_saby)
+    browser.maximize_window()
+
+    # Находим и открываем раздел контакты
 
     contact = browser.find_element(By.LINK_TEXT, 'Контакты')
     contact.click()
-    time.sleep(2)
+    time.sleep(1)
 
-    tensor_banner = browser.find_element(By.CSS_SELECTOR, '.sbisru-Contacts__logo-tensor')
+    # Находим и открываем баннер "Тензор"
+
+    tensor_banner = browser.find_element(By.CSS_SELECTOR, 'a[href="https://tensor.ru/"]')
     tensor_banner.click()
-    time.sleep(2)
+    time.sleep(1)
+
+
+    # Ссылка открылась в новой вкладке, переключаемся на работу в ней
+
     handles = browser.window_handles
     browser.switch_to.window(handles[1])
 
-    power_in_people = browser.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4')
+    # Находим баннер "Сила в людях", пролистываю до него, а то не видно на видео
+    # Проверяем его отображение
+
+    power_in_people = browser.find_element(By.CSS_SELECTOR, 'div[class="s-Grid-container tensor_ru-Index__block4"]')
     power_in_people.location_once_scrolled_into_view
-    time.sleep(2)
-
     power_in_people.is_displayed()
-    more = browser.find_element(By.CSS_SELECTOR, 'a[href*="/about"]')
+    time.sleep(1)
+
+    # Переходим в подробнее
+    # Проверяем ссылку
+
+    more = browser.find_element(By.CSS_SELECTOR, 'a[href="/about"]')
     more.click()
-    time.sleep(2)
-
-
-
-
-
-
-
+    saby_more = "https://tensor.ru/about"
+    assert browser.current_url == saby_more
+    time.sleep(1)
 
 finally:
     browser.quit()
--- Homework_11/task_2.py
+++ Homework_11/task_2.py
@@ -4,7 +4,34 @@
 # Убедиться, что сообщение появилось в реестре
 # Удалить это сообщение и убедиться, что удалили
 # Для сдачи задания пришлите код и запись с экрана прохождения теста
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+
+browser = webdriver.Chrome()
+site_saby = "https://fix-online.saby.ru/"
+
+try:
+    browser.get(site_saby)
+    browser.maximize_window()
+
+    user_login, user_password = 'Strutandol', 'пароль123'
+
+    login = browser.find_element(By.CSS_SELECTOR, '.controls-InputBase__nativeField_caretFilled')
+    login.send_keys(user_login)
+
+    enter = browser.find_element(By.CSS_SELECTOR, '.controls-BaseButton__wrapper')
+    enter.click()
+
+    password = browser.find_element(By.CSS_SELECTOR, '.controls-Password__nativeField_caretFilled')
+    password.send_keys(user_password)
+
+    enter = browser.find_element(By.CSS_SELECTOR, '.controls-BaseButton__wrapper')
+    enter.click()
+
+    time.sleep(5)
+
+finally:
+    browser.quit()
